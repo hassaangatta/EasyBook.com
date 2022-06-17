9454a0c91b9ea7680a4fe0885df6fc63cf98995e
hassaangatta <102753390+hassaangatta@users.noreply.github.com>
2022-06-17 20:27:16 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,51 +5,137 @@
 #include<string.h>
 #include<windows.h>
 using namespace std;
-// #include "D:\EasyBook.com\Travel\travel.h"
-//#include "travel/Flight/Flight.h"
-// #include "travel/Train/Train.h"
-// #include "D:\EasyBook.com\Residence\Residence.h"
+#include "D:\EasyBook.com\Travel\travel.h"
+#include "D:\EasyBook.com\travel\Flight\Flight.h"
+#include "D:\EasyBook.com\travel\Buses\Bus.h"
+#include "D:\EasyBook.com\travel\Train\Train.h"
+#include "D:\EasyBook.com\Residence\Residence.h"
 #include "D:\EasyBook.com\User\user.h"
+#include "D:\EasyBook.com\Admin\admin.h"
+//#include "D:\EasyBook.com\Travel\travel.h"
 int AfterLogIn(user a){
 	char ch;
-	cout<<"1) BOOK TRAIN "<<endl;
-	cout<<"2) BOOK FLIGHT "<<endl;
-	cout<<"3) BOOK BUS "<<endl;
-	cout<<"4) BOOK RESIDENCE "<<endl;
-	cout<<"5) VIEW PREVIOUS RECORDS "<<endl;
-	cout<<"6) EXIT"<<endl;
-	cout<<endl<<endl<<"ENTER CHOICE: ";
-	cin>>ch;
-	switch(ch){
-		case '1':
-			a.trainBook();
+	while(1){
+		system("cls");
+		cout<<"1) BOOK TRAIN "<<endl;
+		cout<<"2) BOOK FLIGHT "<<endl;
+		cout<<"3) BOOK BUS "<<endl;
+		cout<<"4) BOOK RESIDENCE "<<endl;
+		cout<<"5) VIEW PREVIOUS RECORDS "<<endl;
+		cout<<"6) EXIT"<<endl;
+		cout<<endl<<endl<<"ENTER CHOICE: ";
+		cin>>ch;
+		switch(ch){
+			case '1':
+				a.trainBook();
+				break;
+			case '2':
+				a.flightBook();
+				break;
+			case '3':
+				a.busBook();
+				break;
+				//return 0;
+			case '4':
+				a.ResidenceBook();
+				break;
+				//return 0;
+			case '5':
+				a.PreviousRecord();
+				getch();
+				break;
+				//return 0;
+			case '6':
+				return 0;
+			default:
+				cout<<"INVALID CHOICE....";
+				//return 0;						
+		}
+	}
+	
+}
+void AdminAfterLog(admin obj){
+	char ch;
+	while(1){
+		cout<<"1)Add"<<endl;
+		cout<<"2)Delete"<<endl;
+		cout<<"3)Update"<<endl;
+		cout<<"4)Exit";
+		cout<<endl<<endl<<"ENTER CHOICE: ";
+		cin>>ch;
+		if(ch == '1'){
+			system("cls");
+			cout<<"1)FLIGHT"<<endl;
+			cout<<"2)TRAIN"<<endl;
+			cout<<"3)BUS"<<endl;
+			cout<<"4)RESIDENCE"<<endl;
+			cout<<endl<<endl<<"ENTER CHOICE: ";
+			cin>>ch;
+			switch (ch){
+				case '1':
+					obj.flightadd();
+					break;
+				case '2':
+					obj.trainadd();
+					break;
+				case '3':
+					obj.busadd();
+					break;
+				case '4':
+					obj.residenceadd();		
+					break;
+			}
+		}else if(ch == '2'){
+			system("cls");
+			cout<<"1)FLIGHT"<<endl;
+			cout<<"2)TRAIN"<<endl;
+			cout<<"3)BUS"<<endl;
+			cout<<"4)RESIDENCE"<<endl;
+			cout<<endl<<endl<<"ENTER CHOICE: ";
+			cin>>ch;
+			switch (ch){
+				case '1':
+					obj.flightdelete();
+					break;
+				case '2':
+					obj.traindelete();
+					break;
+				case '3':
+					obj.busdelete();
+					break;
+				case '4':
+					obj.residencedelete();		
+					break;
+			}
+		}else if(ch == '3'){
+			system("cls");
+			cout<<"1)FLIGHT"<<endl;
+			cout<<"2)TRAIN"<<endl;
+			cout<<"3)BUS"<<endl;
+			cout<<"4)RESIDENCE"<<endl;
+			cout<<endl<<endl<<"ENTER CHOICE: ";
+			cin>>ch;
+			switch (ch){
+				case '1':
+					obj.flightPriceUpdate();
+					break;
+				case '2':
+					obj.trainPriceUpdate();
+					break;
+				case '3':
+					obj.busPriceUpdate();
+					break;
+				case '4':
+					obj.residencePriceUpdate();		
+					break;
+			}
+		}else if(ch == '4'){
 			break;
-		case '2':
-			a.flightBook();
-			break;
-		case '3':
-			a.busBook();
-			break;
-			return 0;
-		case '4':
-			a.ResidenceBook();
-			break;
-			return 0;
-		case '5':
-			a.PreviousRecord();
-			break;
-			return 0;
-		case '6':
-			return 0;
-		default:
-			cout<<"INVALID CHOICE....";
-			return 0;						
+		}
 	}
 }
 int main ()
 {
-<<<<<<< Updated upstream
-    user a;
     int chk;
     char choice;
 	cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
@@ -59,43 +145,43 @@ int main ()
 	cout<<"PRESS ANY KEY TO CONTINUE"<<endl;
 	getch();
 	system("cls");
-	cout<<"IF NEW HERE ENTER 1 FOR SIGN UP else press any key: ";
+	cout<<"1) Admin"<<endl;
+	cout<<"2)User"<<endl;
 	cin>>choice;
 	if(choice == '1'){
-		a.SignUp();
-		AfterLogIn(a);
-	}else
-	{
-		a.LogIn();
-		AfterLogIn(a);
+		admin obj;
+		chk = obj.logIn();
+		//cout<<chk<<endl;
+		while(1){
+			if(chk == 1){
+				//cout<<"con";
+				system("cls");
+				AdminAfterLog(obj);
+				break;
+			}else
+			{
+				obj.logIn();
+			}
+		}
+	}else{
+		user a;
+		cout<<"SIGUP OR LOG IN 1 FOR SIGN UP: ";
+		cin>>choice;
+		if(choice == '1'){
+			a.SignUp();
+			system("cls");
+			AfterLogIn(a);
+		}else
+		{
+			chk = a.LogIn();
+			if(chk == 1){
+				AfterLogIn(a);
+			}else
+			{
+				system("cls");
+				a.LogIn();
+			}
+		}
 	}
-=======
-    // Flight f;
-    // Travel *T = &f;
-    // T->input();
-    // f.creatFileName();
-    // f.FileRead();
-    // Train t;
-    // Travel *a=&t;
-    // a->input();
-    // t.creatFileName();
-    // t.FileRead();
-    // Residence r;
-    // r.input();
-    // r.creatFileName();
-    // r.FileRead();
-    admin a;
-    // a.flightadd();
-    // a.trainadd();
-    // a.busadd();
-    // a.residenceadd();
-    // a.residencedelete();
-    // a.residencePriceUpdate();
-    // a.flightdelete();
-    // a.traindelete();
-    // a.busdelete();
-    // a.flightPriceUpdate();
-    // a.trainPriceUpdate();
-    a.busPriceUpdate();
->>>>>>> Stashed changes
 }
+
