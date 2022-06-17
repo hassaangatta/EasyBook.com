8fa6d0c766b24cd7f5c3681500c90e98f76cc299
hassaangatta <102753390+hassaangatta@users.noreply.github.com>
2022-06-17 20:42:22 +0500

    Update Flight.h

--- Travel/Flight/Flight.h
+++ Travel/Flight/Flight.h
@@ -2,10 +2,10 @@
 #include <string.h>
 #include <fstream>
 #include <iostream>
+#include<ctime>
 using namespace std;
 class Flight: public Travel
 {
-    protected:
         //string type;
         char fileName [50]="D:\\EasyBook.com\\Travel\\Flight\\";
     public:
@@ -13,39 +13,16 @@ class Flight: public Travel
         {
             return fileName;
         }
-<<<<<<< Updated upstream
         void createFileName ()
-=======
-        void input ()
-        {
-            cout<<"From? :";
-            cin>>from;
-            cout<<"Where? :";
-            cin>>whereTo;
-            cout<<"When? (Date format mm/dd/yyyy):";
-            cin>>when;
-            cout<<"Select Class(Economy/Buisness/First):";
-            fflush(stdin);
-            getline(cin,type);
-        }
-        void input (int)
-        {
-            cout<<"From? :";
-            cin>>from;
-            cout<<"Where? :";
-            cin>>whereTo;
-        }
-        void creatFileName ()
->>>>>>> Stashed changes
         {
             strcat(fileName,from);
             strcat(fileName,"-");
             strcat(fileName,whereTo);
             strcat(fileName,".txt");
-            FileRead();
         }
         void FileRead ()
         {
+			int flag=0;
             ifstream myfile;
             myfile.open(fileName);
             if (myfile.fail())
@@ -63,6 +40,7 @@ class Flight: public Travel
                 //cout<<endl;
                 if (text[2]==when && text[6]==type && stoi(text[5])>seats)
                 {
+					flag=1;
                     for (int i=0;i<8;i++)
                     {
                         cout<<text[i]<<endl;
@@ -70,9 +48,12 @@ class Flight: public Travel
                     cout<<endl<<endl<<endl<<endl;
                 }
             }
-            book();
+			if (flag==0)
+			{
+				cout<<"NO FLIGHT AVAILABLE OF YOUR ENTERED DATA...."<<endl;
+			}
         }
-        void book(){
+        int book(){
         	int count,fileSeats,confirmation;
         	string ch;
         	cout<<"ENTER ID TO BOOK OR ENTER 0 TO EXIT: ";
@@ -145,25 +126,112 @@ class Flight: public Travel
 					temp.close();
 					remove("temp.txt");
 					cout<<endl<<endl;
-					cout<<"FINAL BILL = "<<price*seats;	
+					cout<<"FINAL BILL = "<<price*seats<<endl;	
 				}		
 			}
+			return confirmation;
+		}
+		void input (char)
+		{
+			cout<<"From? :";
+            cin>>from;
+            cout<<"Where? :";
+            cin>>whereTo;
+		}
+		void input(int){
+			int dateValid,mm,yy,dd;
+        	string m,d,y;
+			cout<<"From? :";
+            cin>>from;
+            cout<<"Where? :";
+            cin>>whereTo;
+			while(dateValid != 1){
+            	cout<<"When? (Date format mm/dd/yyyy):";
+            	cin>>when;
+            	if(when > todaysDate()){
+            		m = when[0];
+            		m += when[1];
+            		d = when[3];
+            		d += when[4];
+            		y = when[6];
+            		y += when[7];
+            		y += when[8];
+            		y += when[9];
+            		mm = stoi(m);
+            		dd = stoi(d);
+            		yy = stoi(y);
+//            		cout<<mm<<endl<<dd<<endl<<yy<<endl;
+//            		cout<<(mm >= 1 && mm <= 12)<<endl;
+//            		cout<<(dd >= 1 && dd <= 31)<<endl;
+//            		cout<<(yy >= 2022 && yy <= 2100)<<endl;
+            		if((mm >= 1 && mm <= 12) && (dd >= 1 && dd <= 31) && (yy >= 2022 && yy <= 2100)){
+            			dateValid = 1;
+					}else
+					{
+						cout<<"INVALID DATE...."<<endl;
+						continue;
+					}
+				}else if(when == todaysDate())
+				{
+					cout<<"SAME DAY BOOKING IS NOT POSSIBLE.."<<endl;
+					continue;
+				}else if(when < todaysDate()){
+					cout<<"DATE ALREADYY PASSED..."<<endl;
+				}
+			}
+			cout<<"Select Class(Economy/Buisness/First):";
+            fflush(stdin);
+            getline(cin,type);
 		}
 		void input ()
         {
+        	int dateValid,mm,yy,dd;
+        	string m,d,y;
             cout<<"From? :";
             cin>>from;
             cout<<"Where? :";
             cin>>whereTo;
             cout<<"NUMBER OF SEATS: ";
             cin>>seats;
-            cout<<"When? (Date format mm/dd/yyyy):";
-            cin>>when;
+            while(dateValid != 1){
+            	cout<<"When? (Date format mm/dd/yyyy):";
+            	cin>>when;
+            	if(when > todaysDate()){
+            		m = when[0];
+            		m += when[1];
+            		d = when[3];
+            		d += when[4];
+            		y = when[6];
+            		y += when[7];
+            		y += when[8];
+            		y += when[9];
+            		mm = stoi(m);
+            		dd = stoi(d);
+            		yy = stoi(y);
+//            		cout<<mm<<endl<<dd<<endl<<yy<<endl;
+//            		cout<<(mm >= 1 && mm <= 12)<<endl;
+//            		cout<<(dd >= 1 && dd <= 31)<<endl;
+//            		cout<<(yy >= 2022 && yy <= 2100)<<endl;
+            		if((mm >= 1 && mm <= 12) && (dd >= 1 && dd <= 31) && (yy >= 2022 && yy <= 2100)){
+            			dateValid = 1;
+					}else
+					{
+						cout<<"INVALID DATE...."<<endl;
+						continue;
+					}
+				}else if(when == todaysDate())
+				{
+					cout<<"SAME DAY BOOKING IS NOT POSSIBLE.."<<endl;
+					continue;
+				}else if(when < todaysDate()){
+					cout<<"DATE ALREADYY PASSED..."<<endl;
+				}
+			}
             cout<<"Select Class(Economy/Buisness/First):";
             fflush(stdin);
             getline(cin,type);
 //            cout<<from<<endl<<whereTo;
-            createFileName();
+            //createFileName();
         }
         void FileWrite (int ID,string name,string time,string rs,string seats,string features)
         {
@@ -179,4 +247,39 @@ class Flight: public Travel
             myfile<<features<<endl;
             myfile.close();
         }
+        string todaysDate(){
+        	string date,month,year,todaysDate,mon;
+			time_t curr_time;
+			curr_time = time(NULL);
+			string tm = ctime(&curr_time);
+			month=tm.substr(4,3);
+			date=tm.substr(8,2);
+			year=tm.substr(20,4);
+			if (month=="Jan")
+			mon="01";
+			else if (month=="Feb")
+			mon="02";
+			else if (month=="Mar")
+			mon="03";
+			else if (month=="Apr")
+			mon="04";
+			else if (month=="May")
+			mon="05";
+			else if (month=="Jun")
+			mon="06";
+			else if (month=="Jul")
+			mon="07";
+			else if (month=="Aug")
+			mon="08";
+			else if (month=="Sep")
+			mon="09";
+			else if (month=="Oct")
+			mon="10";
+			else if (month=="Nov")
+			mon="11";
+			else if (month=="Dec")
+			mon="12";
+			todaysDate=mon+"/"+date+"/"+year;
+			return todaysDate;
+		}
 };
